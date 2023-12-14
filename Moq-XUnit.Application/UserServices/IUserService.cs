a0542ca89b4a785cd6cb1869e44a22d259ab1833
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-14 18:18:04 +0500

    nmh C

---
+++ Moq-XUnit.Application/UserServices/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Moq_XUnit.Domain.Dtos;
+using Moq_XUnit.Domain.Entities;
+
+namespace Moq_XUnit.Application.UserServices
+{
+    public interface IUserService
+    {
+        public ValueTask<bool> UserCreateAsync(UserDto userDto);
+        public ValueTask<bool> UserUpdateAsync(int Id , UserDto userDto);
+        public ValueTask<bool> UserDeleteAsync(int Id);
+        public ValueTask<List<User>> UserGetAllAsync();
+        public ValueTask<User> UserGetByIdAsync(int Id);
+    }
+}
