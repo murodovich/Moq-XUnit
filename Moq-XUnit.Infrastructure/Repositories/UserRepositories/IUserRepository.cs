92066de5812727384e8aa19c7034d3dd0f8f4055
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-14 18:19:04 +0500

    dadsadsa

---
+++ Moq-XUnit.Infrastructure/Repositories/UserRepositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Moq_XUnit.Domain.Dtos;
+using Moq_XUnit.Domain.Entities;
+
+namespace Moq_XUnit.Infrastructure.Repositories.UserRepositories
+{
+    public interface IUserRepository
+    {
+        public ValueTask<bool> CreateAsync(UserDto userDto);
+        public ValueTask<bool> UpdateAsync(int Id,UserDto userDto);
+        public ValueTask<bool> DeleteAsync(int Id);
+        public ValueTask<List<User>> GetAllAsync();
+        public ValueTask<User> GetByIdAsync(int Id);
+
+    }
+}
