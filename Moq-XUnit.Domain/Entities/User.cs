3c639c3112262803df23fdb5297216cb40ab9018
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-14 18:18:35 +0500

    User

---
+++ Moq-XUnit.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moq_XUnit.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
