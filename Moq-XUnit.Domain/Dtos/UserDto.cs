6432ed28542c93d467e798f499c0dff694e0bdaf
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-14 18:18:29 +0500

    UserDto

---
+++ Moq-XUnit.Domain/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Moq_XUnit.Domain.Dtos
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+    }
+}
