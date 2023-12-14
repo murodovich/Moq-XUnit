7db2c15bf6628bb60892af587530f302276c02cf
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-14 18:19:00 +0500

    dadsads

---
+++ Moq-XUnit.Infrastructure/Persistance/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq_XUnit.Domain.Entities;
+
+namespace Moq_XUnit.Infrastructure.Persistance
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
