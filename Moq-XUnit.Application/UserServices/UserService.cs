3ab8f958a8f97f64575e852983b11fce7a85adcc
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-14 18:18:16 +0500

    JHSVDAHDB

---
+++ Moq-XUnit.Application/UserServices/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Moq_XUnit.Domain.Dtos;
+using Moq_XUnit.Domain.Entities;
+
+namespace Moq_XUnit.Application.UserServices
+{
+    public class UserService : IUserService
+    {
+        
+        public ValueTask<bool> UserCreateAsync(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> UserDeleteAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<List<User>> UserGetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<User> UserGetByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> UserUpdateAsync(int Id, UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
